ee8fd00cc12312089662b8a17291fa3ac043c94e
A O <ahmedothman8@hotmail.com>
2019-04-24 13:23:51 +0200

    change to sel values

--- FogCaseExam/src/java/DataLayer/FogDataValues.sql
+++ FogCaseExam/src/java/DataLayer/FogDataValues.sql
@@ -17,10 +17,13 @@ select * from roofmaterial;
 
 select * from arearoof;
 
-
-select * from roofmaterial;
 insert into roof (roofmaterialFK, areaFK) values ();
 select * from arearoof;
-insert into
+insert into arearoof (width,length,heigth,degree) values (20,20,15,40);
 
+select * from carport;
+insert into carport (width, length, heigth, materialFK) values (20,20,40,2);
+
+
+select * from shed;
 #REname table arearoof to area;
